c887f76a4ac63ed8a15eaa7333832c0156429bf2
miyakoshi-yuki <miyakoshi2000@icloud.com>
2022-06-09 19:20:49 +0900

    main

---
+++ oled.h
@@ -0,0 +1,6 @@
+#ifndef OLED_H
+#define	OLED_H
+
+void startupScreen(void);
+
+#endif	/* OLED_H */
---
+++ zelda_chest_noise.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   pattern_generator.h
+ * Author: miyakoshi_macbookpro
+ *
+ * Created on May 29, 2022, 5:56 PM
+ */
+
+#ifndef ZELDA_CHEST_NOISE_H
+#define	ZELDA_CHEST_NOISE_H
+
+void playTone(int tone, int duration);
+void playNote(char note, int duration);
+void playMusic();
+
+#endif	/* ZELDA_CHEST_NOISE_H*/
